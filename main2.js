90ed9116f7444d8677e447b8f2d8b6053c03b9ea
Mina Gawargious <minagawargious@utexas.edu>
2020-11-17 14:28:38 -0600

    Added weight editing

--- main2.js
+++ main2.js
@@ -12,6 +12,9 @@ fillColor = "orange";
 textColor = "white";
 let numNodes = 0;
 let sourceNode = newLine = newWeightText = draggedItem = null;
+let selectedRect = document.createElementNS(svgns, "rect");
+setAttributes(selectedRect, {"fill": "none", "stroke": "blue"});
+let selected = null;
 
 let adjacencyList = {}; // startID: [endIds]
 let lines = {}; // startID: [lineObject]
@@ -51,9 +54,9 @@ function getLineProperties(x1, y1, x2, y2){
             dx = -dx;
             dy = -dy;
         }
-        return [x2-dx, y2+dy, "black", "arrowheadBlack_Far", dx, dy]
+        return [x2-dx, y2+dy, "black", "arrowheadBlack_Far"]
     }
-    return [x2, y2, "transparent", "arrowheadBlack_Near", 0, 0]
+    return [x2, y2, "transparent", "arrowheadBlack_Near"]
 }
 
 function updateLabelPosition(x1, y1, x2, y2, label){
@@ -70,7 +73,6 @@ function updateLabelPosition(x1, y1, x2, y2, label){
     setAttributes(label, {"x": labelCenter.x, "y": labelCenter.y, "text-anchor": "middle", "dominant-baseline": "middle"});
 }
 
-// TODO: MOVE LABELS.
 // Used to shift when we create a double connection and when we drag. (x1, y1) is source point, (x2, y2) is end point.
 function updateDoubleConnection(incoming, outgoing, incomingLabel, outgoingLabel, x1, y1, x2, y2){
     let theta = Math.atan((x2-x1)/(y1-y2));
@@ -81,21 +83,15 @@ function updateDoubleConnection(incoming, outgoing, incomingLabel, outgoingLabel
         dy = -dy;
     }
     
-    let incomingX1 = x1 - w*dx, incomingY1 = y1 - w*dy;
-    let [incomingX2, incomingY2, incomingArrowheadColor, incomingArrowhead, incomingXToTip, incomingYToTip] = getLineProperties(x1, y1, x2, y2);
-    incomingX2 -= w*dx;
-    incomingY2 -= w*dy;
+    let [incomingX2, incomingY2, incomingArrowheadColor, incomingArrowhead] = getLineProperties(x1, y1, x2, y2);
 
-    let outgoingX1 = x2 + w*dx, outgoingY1 = y2 + w*dy;
-    let [outgoingX2, outgoingY2, outgoingArrowheadColor, outgoingArrowhead, outgoingXToTip, outgoingYToTip] = getLineProperties(x2, y2, x1, y1);
-    outgoingX2 += w*dx;
-    outgoingY2 += w*dy;
+    let [outgoingX2, outgoingY2, outgoingArrowheadColor, outgoingArrowhead] = getLineProperties(x2, y2, x1, y1);
 
-    setAttributes(incoming, {"x1": incomingX1, "y1": incomingY1, "x2": incomingX2, "y2": incomingY2, "stroke": incomingArrowheadColor, "marker-end": `url(#${incomingArrowhead})`});
-    setAttributes(outgoing, {"x1": outgoingX1, "y1": outgoingY1, "x2": outgoingX2, "y2": outgoingY2, "stroke": outgoingArrowheadColor, "marker-end": `url(#${outgoingArrowhead})`}); 
+    setAttributes(incoming, {"x1": x1 - w*dx, "y1": y1 - w*dy, "x2": incomingX2 - w*dx, "y2": incomingY2 - w*dy, "stroke": incomingArrowheadColor, "marker-end": `url(#${incomingArrowhead})`});
+    setAttributes(outgoing, {"x1": x2 + w*dx, "y1": y2 + w*dy, "x2": outgoingX2 + w*dx, "y2": outgoingY2 + w*dy, "stroke": outgoingArrowheadColor, "marker-end": `url(#${outgoingArrowhead})`}); 
 
-    updateLabelPosition(incomingX1, incomingY1, incomingX2 + incomingXToTip, incomingY2 - incomingYToTip, incomingLabel);
-    updateLabelPosition(outgoingX1, outgoingY1, outgoingX2 + outgoingXToTip, outgoingY2 - outgoingYToTip, outgoingLabel);
+    updateLabelPosition(x1 - w*dx, y1 - w*dy, x2 - w*dx, y2 - w*dy, incomingLabel);
+    updateLabelPosition(x2 + w*dx, y2 + w*dy, x1 + w*dx, y1 + w*dy, outgoingLabel);
 }
 
 function updateSingleConnection(line, label, x1, y1, x2, y2){
@@ -161,12 +157,21 @@ addButton.addEventListener("click", (event) => {
 
             newWeightText = document.createElementNS(svgns, "text");
             newWeightText.textContent = "1";
-            setAttributes(newWeightText, {"x": node.cx.baseVal.value, "y": node.cy.baseVal.value, "class": "disableSelect"});
+            newWeightText.addEventListener("click", (event) => {
+                selected = event.target; // We can reference newWeightText when setting fields upon creation because it's not null then. Upon click, however, it is null, so to reference this unique text field again, use event.target.
+                selected.textContent += "|";
+                
+                setAttributes(selectedRect, {"x": selected.x.baseVal[0].value - selected.getBBox().width/2, "y": selected.y.baseVal[0].value - selected.getBBox().height/2, "width": selected.getBBox().width, "height": selected.getBBox().height});
+                svg.appendChild(selectedRect);
+                editLabel(selected);
+            })
+            setAttributes(newWeightText, {"x": node.cx.baseVal.value, "y": node.cy.baseVal.value});
             svg.appendChild(newLine);  
             svg.appendChild(newWeightText);
         }else if(sourceNode != node && !adjacencyList[sourceNode.id].includes(node.id)){ // Second node to establish connection.
             adjacencyList[sourceNode.id].push(node.id);
             lines[sourceNode.id].push({lineObject: newLine, label: newWeightText});
+            setAttributes(newWeightText, {"node1": sourceNode.id, "node2": node.id});
 
             if(adjacencyList[node.id].includes(sourceNode.id)){ // Outgoing edge already exists. Incoming being created.
                 let index = adjacencyList[node.id].indexOf(`${sourceNode.id}`);
@@ -193,24 +198,74 @@ svg.addEventListener("mousemove", (event) => {
     }
 })
 
+function doneEditing(){
+    if(svg.contains(selectedRect)){
+        svg.removeChild(selectedRect);
+    }
+    if(selected != null){
+        selected.textContent = selected.textContent.replaceAll('|', '');
+        if(selected.textContent.length == 0){
+            selected.textContent = "1";
+        }
+        editLabel(selected);
+        selected = null;
+    }
+}
+
 svg.addEventListener("mousedown", (event) => {
-    draggedItem = event.target;
+    if(event.target.tagName == "circle"){
+        draggedItem = event.target;
+    }
+    doneEditing();
 })
 svg.addEventListener("mouseup", (event) => {
     draggedItem = null;
 })
 
+// Called when we edit a label's text to adjust the position.
+function editLabel(label){
+    let node1Id = label.getAttribute("node1");
+    let node2Id = label.getAttribute("node2");
+    let node1 = svg.getElementById(node1Id);
+    let node2 = svg.getElementById(node2Id);
+
+    let incomingEntry = lines[node1Id][adjacencyList[node1Id].indexOf(node2Id)];
+    if(!adjacencyList[node2Id].includes(node1Id)){ // Single connection.
+        updateSingleConnection(incomingEntry.lineObject, incomingEntry.label, node1.cx.baseVal.value, node1.cy.baseVal.value, node2.cx.baseVal.value, node2.cy.baseVal.value);
+    }else{ // Double connection.
+        let outgoingEntry = lines[node2Id][adjacencyList[node2Id].indexOf(node1Id)];
+        updateDoubleConnection(outgoingEntry.lineObject, incomingEntry.lineObject, outgoingEntry.label, incomingEntry.label, node2.cx.baseVal.value, node2.cy.baseVal.value, node1.cx.baseVal.value, node1.cy.baseVal.value);
+    }
+    setAttributes(selectedRect, {"x": selected.x.baseVal[0].value - selected.getBBox().width/2, "y": selected.y.baseVal[0].value - selected.getBBox().height/2, "width": selected.getBBox().width, "height": selected.getBBox().height});
+}
+
 document.addEventListener("keydown", (event) => {
     if(event.key == "Escape"){
-        if(newLine != null){
-            svg.removeChild(newLine);
+        for (let element of [newLine, newWeightText]){
+            if(element != null && svg.contains(element)){
+                svg.removeChild(element);
+            }
         }
-        if(newWeightText != null){
-            svg.removeChild(newWeightText);
-        }
+        doneEditing();
         sourceNode = newLine = newWeightText = null;
+    }else if (selected != null){
+        // SVG text do not allow editing like input fields do, so I have to mimic it myself with the pipe character as the cursor.
+        let cursorIndex = selected.textContent.indexOf("|");
+        if(event.key == "ArrowLeft" && cursorIndex != 0){ // Move left.
+            selected.textContent = selected.textContent.replace("|","");
+            selected.textContent = selected.textContent.slice(0, cursorIndex - 1) + "|" + selected.textContent.slice(cursorIndex-1);
+        }else if (event.key == "ArrowRight" && cursorIndex != selected.textContent.length-1){
+            selected.textContent = selected.textContent.replace("|","");
+            selected.textContent = selected.textContent.slice(0, cursorIndex + 1) + "|" + selected.textContent.slice(cursorIndex+1);
+        }else if(event.key == "Backspace" || event.key == "Delete" && cursorIndex > 0){
+            selected.textContent = selected.textContent.slice(0, cursorIndex-1) + selected.textContent.slice(cursorIndex);
+            editLabel(selected);
+        }else if (isFinite(event.key) && selected.textContent.length < 6){ // 0 - 9
+            selected.textContent = selected.textContent.slice(0, cursorIndex) + event.key + selected.textContent.slice(cursorIndex);
+            editLabel(selected);
+        }
     }
 });
 
-// TODO: Allow weight editing.
+// TODO: Add start and end options.
 // TODO_IF_TIME: Allow for deleted nodes (delete number 2 of 7, next number is 2, not 8).
